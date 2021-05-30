a1adf5071c1e11bfcfacd71c8522acdab0e2fcc7
kagasan <kaga.masaki.kj4@gmail.com>
2021-05-30 17:08:17 +0900

    update url生成

--- index.js
+++ index.js
@@ -38,6 +38,38 @@ var app = new Vue({
                 columnName: 'userId',
                 format: 'VARCHAR(20)'
             }
-        ]
+        ],
+        url: '',
+        urlText: ''
+    },
+    methods: {
+        makeUrl: function(){
+            this.url = location.href.split('?')[0];
+            this.url += '?databases=' + encodeURI(JSON.stringify(this.databases));
+            this.url += '&columns=' + encodeURI(JSON.stringify(this.columns));
+            const date = new Date();
+            let tmpText = '情報付きurl(最終更新';
+            if(date.getHours() < 10)tmpText += '0';
+            tmpText += date.getHours() + ':';
+            if(date.getMinutes() < 10)tmpText += '0';
+            tmpText += date.getMinutes() + ':';
+            if(date.getSeconds() < 10)tmpText += '0';
+            tmpText += date.getSeconds() + ')';
+            this.urlText = tmpText;
+        }
+    },
+    created: function() {
+        const params = (location.href + '?').split('?')[1].split('&');
+        params.forEach(param => {
+            const key = param.split('=')[0];
+            const value = (param + '=').split('=')[1];
+            if(key === 'databases') {
+                this.databases = JSON.parse(decodeURI(value));
+            }
+            if(key === 'columns') {
+                this.columns = JSON.parse(decodeURI(value));
+            }
+        });
+        this.makeUrl();
     }
 });
